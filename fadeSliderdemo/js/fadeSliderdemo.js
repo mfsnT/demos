6f61ac69ca5cd7f4d61958f571dd8f897b9bd7c6
rjq <mfsnt@qq.com>
2017-10-10 14:23:36 +0800

    new

--- fadeSliderdemo/js/fadeSliderdemo.js
+++ fadeSliderdemo/js/fadeSliderdemo.js
@@ -2,19 +2,14 @@ $(function () {
   var $slider = $('.slider');
   var $imgs_ct = $('.imgs-container');
   var $imgs = $imgs_ct.children();
-  var $first_img = $imgs.first().clone();
   var $prev_btn = $('.prev-btn');
   var $next_btn = $('.next-btn');
   var $bullet = $('.bullet');
   var $bullet_btns = $bullet.children();
-  var width = $imgs_ct.width();
   var index = 0;
   var btn_clock = false;
   var timmer = null;
-  var imgs_len = $imgs.length;
 
-  $imgs_ct.append($first_img);
-  $imgs_ct.css('width', (imgs_len + 1) * width);
 
   $next_btn.on('click', function () {
     if (btn_clock) {
@@ -50,28 +45,16 @@ $(function () {
   function slider() {
     btn_clock = true;
 
-    if (index === -1) {
-      $imgs_ct.css('left', - imgs_len * width);
-      $imgs_ct.animate({left: - (imgs_len - 1) * width}, function () {
-        btn_clock = false;
-        index = 3;
-        showActiveBtn(index);
-      });
-
-      return;
-    } else if (index === imgs_len) {
-      $imgs_ct.animate({left: - index * width}, function () {
-        $(this).css('left', 0);
-        btn_clock = false;
-        index = 0;
-        showActiveBtn(index);
-      });
-
-      return;
+    if (index === 4) {
+      index = 0;
+    } else if (index === -1) {
+      index = 3;
     }
 
-    $imgs_ct.animate({left: - index * width}, function () {
+    $imgs.eq(index).show();
+    $imgs_ct.children('.active-img').fadeOut(function () {
       btn_clock = false;
+      $imgs.removeClass('active-img').eq(index).addClass('active-img');
       showActiveBtn(index);
     });
   }
